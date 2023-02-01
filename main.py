b0841226d838907d2b20bb59eeb5d57dbefca5e9
jakembentley <bentley.jacobm@gmail.com>
2023-02-01 13:59:25 -0600

    updated figures to be independent objects to support easy updates to ingame image

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from uuid import uuid4
 import PySimpleGUI as sg
 import ua_objects as ua
 
+
 def main():
     #define color theme of GUI
     sg.theme('Topanga')
-    sun = ua.Location(3,1,2,None,str(uuid4()))
+    #sun = ua.Location(3,1,2,None)
     path = "location.png"
-    sun.getSelfie(path)
+    #sun["image"].savefig(path)
+    #print(sun["name"])
     #arrange the local map and object description elements in a single column
+    G = ua.gameGraph(100, 0.001)
+    G.image.savefig(path)
     local_col = [
         [sg.Text(key = '-MAP NAME-')],
         [sg.Image(filename = path, key = '-MAP IMAGE-', background_color = '#F9EFE8', size = (1000, 600))],
@@ -30,8 +34,8 @@ def main():
         [game_area]]
     #instantiate a window
     window = sg.Window('Universal Assemblers', layout, size = (1920,1080))
-    print("Suns children: {children}" .format(children = sun["children"]))
-
+    #print("Suns children: {children}" .format(children = sun["children"]))
+    #print("testing parent: {name} and also {child}" .format(name = sun["name"], child = sun["children"][0]["parent"]["name"]))
     while True:
         event, values = window.read()
         print(event, values)
--- ua_objects.py
+++ ua_objects.py
@@ -7,6 +7,8 @@ from matplotlib import pyplot as plt
 from uuid import uuid4
 from matplotlib import patches
 import networkx as nx
+import scipy
+from itertools import combinations,groupby
 
 class Location(dict):
 
@@ -24,8 +26,26 @@ class Location(dict):
         """
         self["children"] = []
         
-        for i in range(num_children):
-            self["children"].append(str(uuid4()))
+        #ensure nested locations are accounted for
+        #if sun determine determine how many are planets (and their moons) and asteroid belts
+        if self['type'] == 1:
+            child_types = random.choices([2,4], weights=[5,1], k=num_children)
+            for i in range(num_children):
+                if child_types[i] == 2:
+                    child = Location(random.randint(0,4), child_types[i], random.randint(1,3), self)
+                    self["children"].append(child)
+                elif child_types[i] == 4:
+                        child = Location(0, child_types[i], None, self)
+                        self["children"].append(child)
+        
+        #if a planet make moons
+        elif self['type'] == 2:
+            child_types = [3 for i in range(num_children)]
+            for i in range(num_children):
+                child = Location(0, child_types[i], None, self)
+                self["children"].append(child)
+
+
         return
 
     def setImage(self):
@@ -35,39 +55,71 @@ class Location(dict):
 
         #draw a larger image of the image itself
         c = plt.Circle((15,15), radius = 3, fill = False)
-        fig, ax = plt.subplots(figsize = (10,6),  dpi = 100, frameon = False)
+        self.figure = mpl.figure.Figure(figsize=(10,6), dpi=100)
+        self.ax = self.figure.add_subplot()
         #set aspects of the axis
-        ax.add_patch(c)
-        ax.grid(False)
-        ax.set_facecolor('#FAF9F6')
-        ax.set_title(self['name'])
-        ax.set_xticklabels([])
-        ax.set_xlim([0,30])
-        ax.set_ylim([0,30])
-        ax.axis('equal')
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_yticklabels([])
+        self.ax.add_patch(c)
+        self.ax.grid(False)
+        self.ax.set_facecolor('#FAF9F6')
+        self.ax.set_title(self['name'])
+        self.ax.set_xticklabels([])
+        self.ax.set_xlim([0,30])
+        self.ax.set_ylim([0,30])
+        self.ax.axis('equal')
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        self.ax.set_yticklabels([])
         #draw children
         #for child n children draw diagonally up or down from self
-        ax.add_patch(c)
-        self["image"] = (fig, ax)
+        self["image"] = self.figure
 
-    def getSelfie(self, figpath):
-        self["image"][0].savefig(figpath)
-        return
 
-    def __init__(self, num_children, loc_type, loc_subtype, parent_ref, ref):
+    def __init__(self, num_children, loc_type, loc_subtype, parent):
+        """
+        location types: 
+        1: Sun
+        2: Planet
+        3: Moon
+        4: Asteroid Belt
+
+        location subtypes:
+        Suns:
+        1: Giant
+        2: Main Sequence
+        3: Dwarf
+        4: Neutron
+        5: Black Hole
+
+        Planets:
+        1: Terrestial
+        2: Gas Giant
+        3: Dwarf
+        
+        No types for Moons or Asteroid Belts
+        """
+
+        #constructor needs to also construct children also exist?
+        #base case: location is either 3 or 4
+        #otherwise also need to construct children
+
         self.setName()
         self.setImage()
-        self.setChildren(num_children)
         self["type"] = loc_type
         self["subtype"] = loc_subtype
-        self["parent"] = parent_ref
-        self["ref"] = 
+        self["parent"] = parent
 
+        if num_children == 0:
+            return
+
+        self.setChildren(num_children)
+
+        return
 
 class gameGraph:
+    """
+    class to manage the logic of the game's nested location graph
+    at the top level solar systems -> planets and asteroid belts -> moons
+    """
     def gnp_random_connected_graph(self, n, p):
         """
         Generates a random undirected graph, similarly to an Erdős-Rényi 
@@ -89,8 +141,41 @@ class gameGraph:
                     G.add_edge(*e)
         return G
 
+    def setGraphImage(self):
+        print("initalizing map image...")
+        fig = mpl.figure.Figure()
+        ax = fig.add_subplot()
+        nx.draw(self.graph, node_color = 'lightblue', with_labels = False, node_size = 20, ax=ax)
+        self.image = fig
+        return
+
+    def setNodeIds(self):
+        #
+        for node in self.graph.nodes:
+            node_id = id(node)
+            if 'id' not in self.graph.nodes[node]:
+                self.graph.nodes[node]['id'] = node_id
+        return
+
+    def setNodeLocations(self):
+        """
+        sets node locations
+        note: node IDS must be initialized
+        """
+
+        print("defining map locations...")
+        for node in self.graph.nodes:
+            if 'location' not in self.graph.nodes[node]:
+                self.graph.nodes[node]['location'] = Location(random.randint(1,12), 1, random.choices([1,2,3,4,5], weights =[10,10,10,1,1]), None)
+
+
     def __init__(self, n, p):
         self.graph = self.gnp_random_connected_graph(n,p)
+        self.setGraphImage()
+        self.setNodeIds()
+        self.setNodeIds()
+        self.setNodeLocations()
 
-       
-        
+
+
+
